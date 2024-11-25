0a06ab162da57ead9119e4e686ce1410139ffb4e
mikkvorning <mikkv@tdc.dk>
2024-11-25 20:28:38 +0100

    More basic project configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme.ts';
 import CssBaseline from '@mui/material/CssBaseline';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
-    <CssBaseline />
+    <CssBaseline enableColorScheme />
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   </ThemeProvider>
 );
