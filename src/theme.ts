a72c3b496a3f19093bda8559442898342498707f
mikkvorning <mikkv@tdc.dk>
2024-11-25 20:28:53 +0100

    Initial theme changes

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,64 @@
-// src/theme.ts
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
-      main: '#1976d2',
+      main: '#02fb4a',
     },
     secondary: {
-      main: '#dc004e',
+      main: '#b700ff',
+    },
+    background: {
+      paper: '#191331',
+      default: '#04050c',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#ffffff',
+    },
+    info: {
+      main: '#2196f3',
+    },
+    success: {
+      main: '#00ad4d',
+    },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#191331',
+          color: '#fff',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px',
+          backgroundColor: '#1976d2',
+          color: '#ffffff',
+          '&:hover': {
+            backgroundColor: '#1565c0',
+          },
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#2e2e2e',
+          color: '#ffffff',
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: '#ffffff',
+        },
+      },
     },
   },
 });
